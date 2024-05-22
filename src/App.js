ca6a6e420a54c7efd9923b29da0e20bf38e20611
Aman <amandwdi@gmail.com>
2024-05-22 17:36:51 +0530

    Table ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
+import axios from "axios";
+import { Table } from "antd";
 
 function App() {
+  const [data , setData] = useState([]);
+
+
+  useEffect(()=>{
+    axios.get("https://jsonplaceholder.typicode.com/users")
+    .then((res)=> 
+      setData(res.data))
+  })
+  const dataSource = data.map(e=>({
+      key: e.id,
+      Name: e.name,
+      Username: e.username,
+      Email: e.email
+  }));
+  
+  const columns = [
+    {
+      title: 'key',
+      dataIndex: 'key',
+      key: 'key',
+    },
+    {
+      
+      title: 'Name',
+      dataIndex: 'Name',
+      key: 'Name',
+    },
+    {
+      title: 'Username',
+      dataIndex: 'Username',
+      key: 'Username',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'Email',
+      key: 'Email',
+    },
+  ];
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table dataSource={dataSource} columns={columns} />;
     </div>
+    
   );
 }
 
